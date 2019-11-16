4ffd7af6a94c0ba792f4142835f00c2a9c5773a9
daniel <opodaniel@yahoo.com>
2019-11-16 22:41:10 +0200

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
 import { deletePuppy, getPuppies, getPuppy, reStore } from './middleware/puppies';
 
 const app = express();
-
+app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+app.use((req, res) => {
+    console.log(req.body); 
+    res.on("finish", () => {
+      console.log(res);
+    });
+  });
 
 app.get('/', (_req, res) => {
     res.send('Hello Buba3!');
